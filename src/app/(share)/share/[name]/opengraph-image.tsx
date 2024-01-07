473590bb2b49d78c0f4ea14678c8b48abed483a5
HemendraKhatik <hemendra.khatik010698@gmail.com>
2024-01-08 00:27:24 +0530

    fix-1

--- src/app/(share)/share/[name]/opengraph-image.tsx
+++ src/app/(share)/share/[name]/opengraph-image.tsx
@@ -1,16 +1,11 @@
 import { PreviewImage } from "@/components/PreviewImage";
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
-
-export const alt = "Demo for previw link";
-
 export const size = {
-  width: 1200,
-  height: 630,
+  width: 256,
+  height: 256,
 };
 
-export const contentType = "image/png";
 
 export default async function Image({ params }: { params: { name: string } }) {
   return new ImageResponse(<PreviewImage name={params.name} />);
