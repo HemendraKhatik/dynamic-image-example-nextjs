70af6fac6c2daced6bc65c7f00dd6f229d4540a9
HemendraKhatik <hemendra.khatik010698@gmail.com>
2024-01-08 09:06:38 +0530

    testing

---
+++ src/app/(share)/share/[name]/hide.tsx
@@ -0,0 +1,11 @@
+// import { PreviewImage } from "@/components/PreviewImage";
+// import { ImageResponse } from "next/og";
+
+// export const size = {
+//   width: 256,
+//   height: 256,
+// };
+
+// export default async function Image({ params }: { params: { name: string } }) {
+//   return new ImageResponse(<PreviewImage name={params.name} />);
+// }
